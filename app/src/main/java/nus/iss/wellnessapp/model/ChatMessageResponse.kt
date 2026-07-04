f589d4f16bb0abe0b9dc1faea5f1bd88478a93bf
Htet Nandar <htetnandar.282198@gmail.com>
2026-07-04 21:13:17 +0800

    Rename role to senderRole in ChatMessageResponse

--- app/src/main/java/nus/iss/wellnessapp/model/ChatMessageResponse.kt
+++ app/src/main/java/nus/iss/wellnessapp/model/ChatMessageResponse.kt
@@ -3,7 +3,7 @@ package nus.iss.wellnessapp.model
 
 data class ChatMessageResponse(
     val id: Long,
-    val role: String,     // "user" or "assistant"
+    val senderRole: String,   // "user" or "assistant"
     val content: String,
     val createdAt: List<Int>
 )
