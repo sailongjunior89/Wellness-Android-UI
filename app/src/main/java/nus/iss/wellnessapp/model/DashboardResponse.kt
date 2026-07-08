702aedce2d642312fc81cc2564565aec095e990f
cecilzhou221 <cecil.chew221@gmail.com>
2026-07-08 19:16:49 +0800

    Add Wellness Scores to Dashboard

--- app/src/main/java/nus/iss/wellnessapp/model/DashboardResponse.kt
+++ app/src/main/java/nus/iss/wellnessapp/model/DashboardResponse.kt
@@ -12,10 +12,24 @@ data class DashboardResponse(
     @SerializedName("waterIntake") val waterIntake: Double,
     @SerializedName("steps") val steps: Double,
     @SerializedName("mood") val mood: String,
+
     @SerializedName("latestRecommendation") val latestRecommendation: String,
+    @SerializedName("latestRecommendationText") val latestRecommendationText: String,
+
     @SerializedName("avgExerciseMinutes") val avgExerciseMinutes: Int,
     @SerializedName("avgWaterIntake") val avgWaterIntake: Double,
     @SerializedName("avgSleepHours") val avgSleepHours: Double,
-    @SerializedName("avgSteps") val avgSteps: Double
+    @SerializedName("avgSteps") val avgSteps: Double,
+
+    @SerializedName("overallWellnessScore") val overallWellnessScore: Int,
+    @SerializedName("stepsScore") val stepsScore: Int,
+    @SerializedName("sleepScore") val  sleepScore: Int,
+    @SerializedName("exerciseScore") val exerciseScore: Int,
+    @SerializedName("waterScore") val waterScore: Int,
+
+    @SerializedName("sleepRecordDate") val sleepRecordDate: String,
+    @SerializedName("exerciseRecordDate") val exerciseRecordDate: String,
+    @SerializedName("waterRecordDate") val waterRecordDate: String,
+    @SerializedName("stepsRecordDate") val stepsRecordDate: String,
 
 )
